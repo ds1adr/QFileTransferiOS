57de098c6c32e533e5577cb66fcf2fae262a76df
Wontai Ki <ds1adr@gmail.com>
2026-03-29 08:11:17 -0700

    Initial Project

---
+++ QFileTransfer-iOS/QFileTransfer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  QFileTransfer_iOSApp.swift
+//  QFileTransfer-iOS
+//
+//  Created by Wontai Ki on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct QFileTransfer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
